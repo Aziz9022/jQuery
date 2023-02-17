c9ec18d88bf2201e9c47c980c0665cf489e3e008
Aziz <kanoorabdulaziz@gmail.com>
2023-02-17 21:11:30 +0530

    Animation

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+
+   // Fade Functions
+
    $("#btn1").click(function (e) {
       $("h1").fadeOut(1000);
    });
@@ -15,8 +18,12 @@ $(document).ready(function () {
       $("h1").fadeTo(1000, 0.5);
    });
 
+   // Slide Function
+
    $("#btn5").click(function (e) {
-      $("h1").slideUp();
+      $("h1").slideUp(1000,function () {  
+         alert('done');
+      });
    });
 
    $("#btn6").click(function (e) {
@@ -26,5 +33,17 @@ $(document).ready(function () {
    $("#btn7").click(function (e) {
       $("h1").slideToggle();;
    });
+
+   // Animate Function -> animate({params} , speed , callback)
+
+   $("#btn8").click(function (e) {
+      $("h1").animate({width:"+=300" , height: "+=300" , left : "200px"},300, function () {  
+         $("h1").animate(
+            {width:"300px" , height: "300px" , left:"0", borderRadius : "10px"},300
+         );
+      });
+   });
+
+
 
 });
