5ef6d60d68907947a55942163b81c4aab521037d
Aziz <kanoorabdulaziz@gmail.com>
2023-02-17 20:53:03 +0530

    20:52 17-02-2023

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 $(document).ready(function () {
-    $("#btn").html("");
-    $("#btn").html("");
-    $("#btn").html("");
+   $("#btn1").click(function (e) {
+      $("h1").fadeOut(1000);
+   });
+  
+   $("#btn2").click(function (e) {
+      $("h1").fadeIn();
+   });
+
+   $("#btn3").click(function (e) {
+      $("h1").fadeToggle();
+   });
+
+   $("#btn4").click(function (e) {
+      $("h1").fadeTo(1000, 0.5);
+   });
+
+   $("#btn5").click(function (e) {
+      $("h1").slideUp();
+   });
+
+   $("#btn6").click(function (e) {
+      $("h1").slideDown();
+   });
+
+   $("#btn7").click(function (e) {
+      $("h1").slideToggle();;
+   });
 
 });
